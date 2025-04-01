4853072b6d3daa251ed1f1c5c88098ac630c409f
varvaratikh <tikhonovavarvara181203@gmail.com>
2025-04-01 09:24:10 +0300

    added new task

--- src/array/index.ts
+++ src/array/index.ts
@@ -315,6 +315,41 @@ const zeroOneSort = (arr: number[]): number[] => {
 //      Вход: [-1, 0, 1, 2, -1, -4]
 //      Выход: [[-1, -1, 2], [-1, 0, 1]]
 //      ```
+
+
+const returnTrioWithANullSum = (nums: number[]): number[][] => {
+    const result: number[][] = [];
+
+    nums.sort((a, b) => a - b);
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right];
+
+            if (sum === 0) {
+                result.push([nums[i], nums[left], nums[right]]);
+                left++;
+                right--;
+
+                while (left < right && nums[left] === nums[left - 1]) left++;
+                while (left < right && nums[right] === nums[right + 1]) right--;
+            } else if (sum < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+
+    return result;
+}
+
+
 //
 // ---
 //
