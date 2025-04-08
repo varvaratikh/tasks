1781f36a1df7973e8220c0aac516575e5e140408
varvaratikh <tikhonovavarvara181203@gmail.com>
2025-04-08 09:23:06 +0300

    added new task

--- src/HashMap_Set/index.ts
+++ src/HashMap_Set/index.ts
@@ -15,6 +15,29 @@ const checkForDuplicates = (arr: number[]) : boolean => {
 // Условие: Являются ли две строки анаграммами друг друга?
 // Тип: HashMap или Array[26] для частот символов
 // Пример: "listen", "silent" → true
+
+
+const isAnagram = (str1: string, str2: string): boolean => {
+    if (str1.length !== str2.length) return false;
+
+    const charCount = new Map<string, number>();
+
+    for (const char of str1) {
+        charCount.set(char, (charCount.get(char) || 0) + 1);
+    }
+
+    for (const char of str2) {
+        if (!charCount.has(char)) return false;
+        charCount.set(char, charCount.get(char)! - 1);
+        if (charCount.get(char)! < 0) return false;
+    }
+
+    return true;
+}
+
+
+
+
 // 3)Частота символов
 // Условие: Верни мапу с количеством вхождений каждого символа в строку.
 // Пример: "aabbc" → {a: 2, b: 2, c: 1}
